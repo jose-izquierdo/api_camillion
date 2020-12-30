bc783b63de669da74fb0a5d37b0f2aeddf2d9c57
José Izquierdo <jainguns@gmail.com>
2020-12-30 21:28:35 +0100

    #7 spec for auth token generation. Test on red

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,12 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
     it { should validate_uniqueness_of(:email) }
   end
+
+  describe 'save' do
+    let(:user) {create :user}
+
+    it 'must assign an auth token' do
+      expect(user.authentication_token.blank?).to be false
+    end
+  end
 end
