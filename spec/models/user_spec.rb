2280830c057e942a3340d6d0e536764389b95f00
José Izquierdo <jainguns@gmail.com>
2020-12-30 21:21:41 +0100

    #7 spec validation added for email uniqueness. All red

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { should validate_uniqueness_of(:email) }
+  end
 end
