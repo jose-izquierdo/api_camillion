bc87bc39bd961fcceb856643b274417fc255391e
José Izquierdo <jainguns@gmail.com>
2020-12-30 21:30:56 +0100

    #7 generating auth token correctly. All green

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ApplicationRecord
+  # Callbacks
+  before_save :generate_auth_token
+  
   # Validations
   validates :email, uniqueness: true
+
+  private
+
+  def generate_auth_token
+    token = ''
+
+    loop do
+      token = SecureRandom.hex(10)
+      break token unless User.where(authentication_token: token).first
+    end
+
+    self.authentication_token = token
+  end
 end
