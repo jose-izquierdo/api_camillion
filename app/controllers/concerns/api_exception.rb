53c190eedd291a0801af5557b17e77c57bf43fb9
José Izquierdo <jainguns@gmail.com>
2020-12-31 13:06:22 +0100

    refactor applied for api_exceptions. 404 rescue eliminated. All green

--- app/controllers/concerns/api_exception.rb
+++ app/controllers/concerns/api_exception.rb
@@ -2,10 +2,6 @@ module ApiException
   extend ActiveSupport::Concern
 
   included do
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      render json: errors_json([{ status: 404, code: 404, title: 'Not found' }]), status: :not_found
-    end
-  
     rescue_from ActiveRecord::RecordInvalid do |e|
       render json: errors_json([{ status: 422, code: 422, title: 'unprocessable entity' }]), status: :unprocessable_entity
     end
